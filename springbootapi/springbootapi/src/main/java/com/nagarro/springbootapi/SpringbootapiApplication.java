93c24b7bcd78390175d7d385a07dbd1a90b8cbf3
priyanka <priyankadabral0090@gmail.com>
2024-03-29 15:09:57 +0530

    springboot app

---
+++ springbootapi/springbootapi/src/main/java/com/nagarro/springbootapi/SpringbootapiApplication.java
@@ -0,0 +1,21 @@
+package com.nagarro.springbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootapiApplication.class, args);}
+		
+		@GetMapping("/")
+		public String home() {
+			return "Hello World";
+		
+	}
+
+}
---
+++ springbootapi/springbootapi/src/test/java/com/nagarro/springbootapi/SpringbootapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.springbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
